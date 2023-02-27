2e8cf7f32c0718d68923a289c6b90411851f9a85
Oleg Chekhonadskikh <oleg.chekhonadskikh@philips.com>
2023-02-27 10:28:46 +0200

    Initial commit

---
+++ src/test/java/com/polarbookshop/awscatalogservice/AwsCatalogServiceApplicationTests.java
@@ -0,0 +1,40 @@
+package com.polarbookshop.awscatalogservice;
+
+import com.polarbookshop.awscatalogservice.domain.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class AwsCatalogServiceApplicationTests {
+
+	@Autowired
+	private WebTestClient webTestClient;
+
+	@Test
+	void whenGetRequestWithIdThenBookReturned() {
+		var bookIsbn = "1231231230";
+		var bookToCreate = new Book(bookIsbn, "Title", "Author", 9.90);
+		Book expectedBook = webTestClient
+				.post()
+				.uri("/books")
+				.bodyValue(bookToCreate)
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody(Book.class).value(book -> assertThat(book).isNotNull())
+				.returnResult().getResponseBody();
+
+		webTestClient
+				.get()
+				.uri("/books/" + bookIsbn)
+				.exchange()
+				.expectStatus().is2xxSuccessful()
+				.expectBody(Book.class).value(actualBook -> {
+					assertThat(actualBook).isNotNull();
+					assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn());
+				});
+	}
+}
