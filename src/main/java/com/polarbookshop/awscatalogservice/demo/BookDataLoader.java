544a1fac262d4e1e5daa8395a9bab682062ecbce
Oleg Chekhonadskikh <oleg.chekhonadskikh@philips.com>
2023-03-03 11:50:00 +0200

    add demo 2

--- src/main/java/com/polarbookshop/awscatalogservice/demo/BookDataLoader.java
+++ src/main/java/com/polarbookshop/awscatalogservice/demo/BookDataLoader.java
@@ -21,6 +21,7 @@ public class BookDataLoader {
     public void loadBookTestData() {
         var book1 = new Book("1234567891", "Northern Lights", "Lyra Silverstar", 9.90);
         var book2 = new Book("1234567892", "Polar Journey", "Iorek Polarson", 12.90);
+        var book3 = new Book("1234567893", "Journey Polar", "Polarson Iorek", 90.12);
         bookRepository.save(book1);
         bookRepository.save(book2);
     }
